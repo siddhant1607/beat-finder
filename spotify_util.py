245c32130b988864c21d7fd6a1d36ad0d6c4c820
Siddhant Pandey <80204746+siddhant1607@users.noreply.github.com>
2025-10-29 22:59:07 +0530

    Update spotify_util.py

--- spotify_util.py
+++ spotify_util.py
@@ -3,20 +3,18 @@ from spotipy.oauth2 import SpotifyClientCredentials
 
 def get_spotify_tracks(playlist_url, client_id, client_secret):
     sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=client_id, client_secret=client_secret))
-    tracks = []
     results = sp.playlist_items(playlist_url)
+    tracks = []
     while results:
         for item in results['items']:
             track = item['track']
-            if track is None:
-                continue
-            tracks.append({
-                'title': track['name'],
-                'artist': track['artists'][0]['name'],
-                'duration': track['duration_ms'] / 1000,
-            })
-        # Paginate if needed
-        if results['next']:
+            if track:
+                tracks.append({
+                    'title': track['name'],
+                    'artist': track['artists'][0]['name'],
+                    'duration': track['duration_ms'] / 1000,
+                })
+        if results.get('next'):
             results = sp.next(results)
         else:
             break
